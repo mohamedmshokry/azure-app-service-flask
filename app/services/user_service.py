2c3502ac5221fd3e97f5c6132c417835e7c6fa8f
mohamedmshokry <mohamed.magdyshokry@gmail.com>
2024-11-12 16:19:10 +0200

    Add a new user

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -4,7 +4,8 @@ class UserService:
             {"id": 1, "name": "John Doe"},
             {"id": 2, "name": "Mohamed Shokry"},
             {"id": 3, "name": "Jane Smith"},
-            {"id": 4, "name": "Mohamed Ali"}
+            {"id": 4, "name": "Mohamed Ali"},
+            {"id": 5, "name": "Jason Smith"}
         ]
 
     def get_users(self):
